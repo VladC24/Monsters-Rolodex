0239821afdbe89d56413be0f548541f0817a16e1
VladC <vlad.chiriac24@gmail.com>
2020-02-15 18:00:01 +0000

    Updates

---
+++ precache-manifest.d09049a703f2855aaa9e44bd612147ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e1bedf83458aef9a4a5e490b9168d9f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e6089bb6f5a303b01043",
+    "url": "/monsters-rolodex/static/css/main.92173d53.chunk.css"
+  },
+  {
+    "revision": "5972ab9a24d87de2b217",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.40aee04e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6089bb6f5a303b01043",
+    "url": "/monsters-rolodex/static/js/main.1d744ab1.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
